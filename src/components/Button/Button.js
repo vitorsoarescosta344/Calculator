620f7f6b1c41433bd1671ef006624c11daaa8fc0
Victor Soares <victorsoarescosta344@gmail.com>
2022-11-19 19:31:46 -0300

    keyboard

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,24 @@
-import {TouchableOpacity} from 'react-native';
+import {Icon} from '@rneui/themed';
+import {Dimensions, Text, TouchableOpacity} from 'react-native';
 
-export default function Button() {
+export default function Button({size, icon, operator, text}) {
+  const width = Dimensions.get('screen').width / 4;
+
   return (
     <>
-      <TouchableOpacity />
+      <TouchableOpacity
+        style={{
+          width: width * size,
+          height: width,
+          backgroundColor: operator ? 'orange' : 'gray',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {icon !== undefined && (
+          <Icon type="material-community" size={35} name={icon} />
+        )}
+        {text !== undefined && <Text style={{fontSize: 35}}>{text}</Text>}
+      </TouchableOpacity>
     </>
   );
 }
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,32 @@
 import {View} from 'react-native';
+import Button from '../../components/Button';
 import Container from '../../layout/Container';
 
 export default function HomeScreen() {
   return (
     <>
       <Container>
-        <View></View>
+        <View style={{flexWrap: 'wrap', flexDirection: 'row'}}>
+          <Button size={1} text={'C'} />
+          <Button size={1} icon={'plus-minus-variant'} />
+          <Button size={1} icon={'percent'} />
+          <Button size={1} icon={'division'} operator />
+          <Button size={1} text={'7'} />
+          <Button size={1} text={'8'} />
+          <Button size={1} text={'9'} />
+          <Button size={1} icon={'multiplication'} operator />
+          <Button size={1} text={'4'} />
+          <Button size={1} text={'5'} />
+          <Button size={1} text={'6'} />
+          <Button size={1} icon={'minus'} operator />
+          <Button size={1} text={'1'} />
+          <Button size={1} text={'2'} />
+          <Button size={1} text={'3'} />
+          <Button size={1} icon={'plus'} operator />
+          <Button size={2} text={'0'} />
+          <Button size={1} text={'.'} />
+          <Button size={1} icon={'equal'} operator />
+        </View>
       </Container>
     </>
   );
