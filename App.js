2ab47877ad79181cbe1c07ec4e0fa43c37bdaaef
Victor Soares <victorsoarescosta344@gmail.com>
2022-11-19 19:06:11 -0300

    button

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import {createTheme, ThemeProvider} from '@rneui/themed';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return <HomeScreen />;
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <HomeScreen />
+    </ThemeProvider>
+  );
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import {TouchableOpacity} from 'react-native';
+
+export default function Button() {
+  return (
+    <>
+      <TouchableOpacity />
+    </>
+  );
+}
