ba9a89650d62616f11b4f03fa7368f0a095712e3
Victor Soares <victorsoarescosta344@gmail.com>
2022-11-19 20:09:00 -0300

    adition operation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,43 @@
-import {Icon} from '@rneui/themed';
+import {Icon, useTheme} from '@rneui/themed';
 import {Dimensions, Text, TouchableOpacity} from 'react-native';
 
-export default function Button({size, icon, operator, text}) {
+export default function Button({size, icon, operator, text, feature, onPress}) {
   const width = Dimensions.get('screen').width / 4;
+
+  const {theme} = useTheme();
 
   return (
     <>
       <TouchableOpacity
+        onPress={() => (feature || operator ? onPress() : onPress(text))}
         style={{
           width: width * size,
           height: width,
-          backgroundColor: operator ? 'orange' : 'gray',
+          backgroundColor: operator
+            ? theme.colors.warning
+            : feature
+            ? theme.colors.grey4
+            : theme.colors.grey0,
           alignItems: 'center',
           justifyContent: 'center',
         }}>
         {icon !== undefined && (
-          <Icon type="material-community" size={35} name={icon} />
+          <Icon
+            type="material-community"
+            size={35}
+            color={feature ? theme.colors.black : theme.colors.white}
+            name={icon}
+          />
         )}
-        {text !== undefined && <Text style={{fontSize: 35}}>{text}</Text>}
+        {text !== undefined && (
+          <Text
+            style={{
+              fontSize: 35,
+              color: feature ? theme.colors.black : theme.colors.white,
+            }}>
+            {text}
+          </Text>
+        )}
       </TouchableOpacity>
     </>
   );
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,31 +1,94 @@
-import {View} from 'react-native';
+import {useState} from 'react';
+import {Text, View} from 'react-native';
 import Button from '../../components/Button';
 import Container from '../../layout/Container';
 
 export default function HomeScreen() {
+  const [firstValue, setFirstValue] = useState('0');
+  const [secondValue, setSecondValue] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  function onKeyboardPress(value) {
+    if (firstValue === '0' && operation === '') {
+      setFirstValue(value);
+    } else if (firstValue !== '0' && operation === '') {
+      setFirstValue(old => setFirstValue(old + value));
+    } else if (secondValue === '0' && operation !== '') {
+      setSecondValue(value);
+    } else if (secondValue !== '0' && operation !== '') {
+      setSecondValue(old => setSecondValue(old + value));
+    }
+  }
+
+  function clearHandler() {
+    setFirstValue('0');
+    setSecondValue('0');
+    setOperation('');
+  }
+
+  function resultHandler() {
+    switch (operation) {
+      case '+':
+        setFirstValue(parseFloat(firstValue) + parseFloat(secondValue));
+        setOperation('');
+        setSecondValue('0');
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <>
       <Container>
+        <View
+          style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+          <Text style={{fontSize: 35}}>
+            {operation === '' ? firstValue : secondValue}
+          </Text>
+        </View>
         <View style={{flexWrap: 'wrap', flexDirection: 'row'}}>
-          <Button size={1} text={'C'} />
-          <Button size={1} icon={'plus-minus-variant'} />
-          <Button size={1} icon={'percent'} />
-          <Button size={1} icon={'division'} operator />
-          <Button size={1} text={'7'} />
-          <Button size={1} text={'8'} />
-          <Button size={1} text={'9'} />
-          <Button size={1} icon={'multiplication'} operator />
-          <Button size={1} text={'4'} />
-          <Button size={1} text={'5'} />
-          <Button size={1} text={'6'} />
-          <Button size={1} icon={'minus'} operator />
-          <Button size={1} text={'1'} />
-          <Button size={1} text={'2'} />
-          <Button size={1} text={'3'} />
-          <Button size={1} icon={'plus'} operator />
-          <Button size={2} text={'0'} />
-          <Button size={1} text={'.'} />
-          <Button size={1} icon={'equal'} operator />
+          <Button size={1} text={'C'} feature onPress={clearHandler} />
+          <Button size={1} icon={'plus-minus-variant'} feature />
+          <Button size={1} icon={'percent'} feature />
+          <Button
+            size={1}
+            icon={'division'}
+            operator
+            onPress={() => setOperation('/')}
+          />
+          <Button size={1} text={'7'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'8'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'9'} onPress={text => onKeyboardPress(text)} />
+          <Button
+            size={1}
+            icon={'multiplication'}
+            operator
+            onPress={() => setOperation('*')}
+          />
+          <Button size={1} text={'4'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'5'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'6'} onPress={text => onKeyboardPress(text)} />
+          <Button
+            size={1}
+            icon={'minus'}
+            operator
+            onPress={() => setOperation('-')}
+          />
+
+          <Button size={1} text={'1'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'2'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'3'} onPress={text => onKeyboardPress(text)} />
+          <Button
+            size={1}
+            icon={'plus'}
+            operator
+            onPress={() => setOperation('+')}
+          />
+          <Button size={2} text={'0'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} text={'.'} onPress={text => onKeyboardPress(text)} />
+          <Button size={1} icon={'equal'} operator onPress={resultHandler} />
         </View>
       </Container>
     </>
